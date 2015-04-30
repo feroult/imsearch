ed5fb42340aa6aaee0c27b70f6e5439b7a51d709
feroult <feroult@gmail.com>
2015-04-30 10:45:15 -0300

    line extractor

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-(cd lib; g++ `pkg-config --cflags --libs opencv` surf_flann_matcher.cpp -o ../bin/surf_flann_matcher)
-(cd lib; g++ `pkg-config --cflags --libs opencv` surf_descriptor.cpp -o ../bin/surf_descriptor)
+FILE=$1
+FILENAME=$(basename $FILE)
+NAME="${FILENAME%.*}"
+EXTENSION="${FILENAME##*.}"
+
+g++ `pkg-config --cflags --libs opencv` $FILE -o bin/$NAME
